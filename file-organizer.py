c11cceba1ee4e9789b73280e4d79e5655dacf36e
Eric Eaton <ericthomaseaton@gmail.com>
2022-09-23 16:18:42 -0400

    Create file-organizer script
    Automatically moves pictures and documents to their respective folders
    from the Downloads folder

---
+++ file-organizer.py
@@ -0,0 +1,36 @@
+from watchdog.observer import observer
+from watchdog.events import FileSystemEventHandler
+
+import os
+import json
+import time
+
+downloads_folder = "/home/eric/Downloads/"
+documents_folder = "/home/eric/Documents/unorganized-docs/"
+pictures_folder = "/home/eric/Pictures/unorganized/"
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        for filename in os.listdir(downloads_folder):
+            src = downloads_folder + filename
+            pic_location = pictures_folder + filename
+            docs_location = documents_folder + filename
+            split_file = os.path.splittext(filename)
+            file_type = split_file[1]
+            if file_type = '.png' or '.jpeg' or '.gif':
+                os.rename(src, pic_location)
+            elif file_type = '.docx' or '.doc' or '.pdf':
+                os.rename(src, docs_location)
+            else:
+                continue
+
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, downloads_folder, recursive=True)
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
